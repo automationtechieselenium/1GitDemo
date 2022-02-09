0d183e6b3da910e51795b555fa255c997ce19307
automationtechieselenium <sanjitapatnaik224@gmail.com>
2022-02-09 20:33:15 +0530

    first commit from australia

--- src/main/java/qaclickacademy/MavenTest/RESTAPITest.java
+++ src/main/java/qaclickacademy/MavenTest/RESTAPITest.java
@@ -9,10 +9,8 @@ public class RESTAPITest {
 		System.out.println(" postJira");
 		System.out.println(" postJira2");
 		System.out.println(" postJira3");
-		System.out.println(" postJira 4");
-		System.out.println(" postJira 5 ");
-		System.out.println(" postJira 6");
-		
+		System.out.println(" postJira10");
+		System.out.println(" postJira36");
 	}
 	@Test
 	public void deleteTwitter()
--- src/main/java/qaclickacademy/MavenTest/SeleniumTest.java
+++ src/main/java/qaclickacademy/MavenTest/SeleniumTest.java
@@ -8,7 +8,8 @@ public class SeleniumTest {
 	public void BrowserAutomation()
 	{
 		System.out.println("BrowserAutomation");
-		System.out.println("WebAutomationd");
+		System.out.println("WebAutomation");
+		System.out.println("ApiAutomation");
 	}
 	@Test
 	public void elementsui()
